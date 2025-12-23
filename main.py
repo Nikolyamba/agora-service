d48da6b56b4e0c3c0fd3c87b9b3e4f136723fbf8
Nikolyamba <niko7tkc@yandex.ru>
2025-12-23 14:19:33 +0300

    чутка отрефакторил, добавил healthcheck, вечером займусь агорой

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = FastAPI()
 
 app.include_router(a_router)
 
+@app.get("/health")
+async def healthcheck():
+    return {"status": "ok", "service": "AgoraService"}
+
 if __name__ == '__main__':
     uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level='info', reload=True)
